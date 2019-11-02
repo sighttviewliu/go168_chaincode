811bbf5bdcfd6d40e0ed3e918214f08bb0088c6c
michael_lau <denchane@gmail.com>
2019-11-02 12:41:41 +0800

    title: fmt \n comment: \n or \r

---
+++ spinner_fmt.go
@@ -0,0 +1,30 @@
+package main
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	go spinner(100 * time.Millisecond)
+	const n = 45 
+	fibN := fib(n)
+	fmt.Printf("\nFibonacci(%d) = %d\n", n, fibN)
+}
+
+func spinner(delay time.Duration) { //spin show
+	for {
+		for _, r := range `-\|/` {
+			fmt.Printf("\n%c", r)
+			time.Sleep(delay)
+		}
+	}
+}
+
+
+func fib(x int) int {
+	if x < 2 {
+		return x
+	}
+	return fib(x-1) + fib(x-2)
+}
+
